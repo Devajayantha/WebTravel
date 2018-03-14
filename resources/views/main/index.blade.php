b02a33a5c90b55da8ba6d5ce3862a93515430c95
deva jayantha <devajayantha870@gmail.com>
2018-03-14 13:22:16 +0800

    update layout

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,25 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/apps.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fontawesome-all.css')}}">
     <title>Penida Trip | Tour and Travel</title>
 </head>
 <body>
-        <div class="nav-header" id="nav-header">
-            <ul class="nav-menu">
-                <li class="font-judul">
-                    <a href="apload.php">	
-                        <span>Agenda</span>
-                    </a>
-                </li>
-                <li class="logout">
-                    <a href="logout.php">	
-                        <i class="fa fa-sign-out"></i>
-                        <span>Log Out</span>
-                    </a>
-                </li>
-            </ul>
-            </div>
+    <div class="nav-header" id="nav-header">
+        <ul class="nav-menu">
+            <li class="font-judul">
+                <a href="#">
+                    <i class="fab fa-github-square"></i>	
+                    <span>Home</span>
+                </a>
+            </li>
+            <li class="logout">
+                <a href="#">	
+                    <i class="fab fa-github-square"></i>
+                    <span>Gallery</span>
+                </a>
+            </li>
+             <li class="logout">
+                <a href="logout.php">	
+                    <i class="fab fa-github-square"></i>
+                    <span>Booking Now</span>
+                </a>
+            </li>
+            <li class="btn btn-outline-primary">
+                <a href="#">	
+                    <span>LOGIN</span>
+                </a>
+            </li>
+            <li class="btn btn-outline-success">
+                <a href="#">	
+                    <span>LOGIN</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
