ef85f5b23ef7dbe4b09e84bcb172bc6b1e46d147
Nadia Taradhita <nadiataradhita@Nadias-MacBook-Pro.local>
2018-03-23 07:41:04 +0800

    edit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 Route::get('/main', function () {
     return view('main.index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
